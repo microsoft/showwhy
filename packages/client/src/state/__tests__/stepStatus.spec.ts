12248417525065177a8d9b65eadc42764bfdca83
Dayenne Souza <ddesouza@microsoft.com>
2021-12-08 17:01:35 -0300

    run lint & fix contributors list

--- packages/client/src/state/__tests__/stepStatus.spec.ts
+++ packages/client/src/state/__tests__/stepStatus.spec.ts
@@ -4,8 +4,8 @@
  */
 import { renderHook } from '@testing-library/react-hooks'
 import { RecoilRoot, snapshot_UNSTABLE } from 'recoil'
-import { stepStatusState, useStepStatus } from '../stepStatus'
 import { StepStatus } from '../../common/enums'
+import { stepStatusState, useStepStatus } from '../stepStatus'
 
 describe('stepStatusState', () => {
 	describe('useStepStatus', () => {
