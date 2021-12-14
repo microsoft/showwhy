6ec15b62a57cca3056227f0060f38e3235137266
Monica Carvajal <mcarvajal@microsoft.com>
2021-12-13 21:19:52 -0500

    refactored factors definition form

---
+++ packages/client/src/components/FactorsDefinitionForm/variables.tsx
@@ -0,0 +1,100 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+// const checkbox = useCheckbox(isPrimary, setIsPrimary)
+// const variablePicker = useVariablePicker(variable, setVariable, variables)
+// const descriptionBox = useDescriptionBox(description, setDescription, variable, add, factor)
+import {
+	Checkbox,
+	DefaultButton,
+	IComboBoxOption,
+	TextField,
+} from '@fluentui/react'
+import { useMemo } from 'react'
+import styled from 'styled-components'
+import { VariablePicker } from '~components/VariablePicker'
+import { Factor } from '~interfaces'
+
+import { GenericFn } from '~types'
+
+export const useCheckbox = (
+	isPrimary: boolean,
+	setIsPrimary: GenericFn,
+): JSX.Element => {
+	return useMemo(() => {
+		return (
+			<Checkbox
+				label="Is primary?"
+				checked={isPrimary}
+				onChange={(_, value) => setIsPrimary(value)}
+			/>
+		)
+	}, [isPrimary, setIsPrimary])
+}
+
+export const useVariablePicker = (
+	variable: string,
+	setVariable: GenericFn,
+	variables?: IComboBoxOption[],
+): JSX.Element => {
+	return useMemo(() => {
+		return (
+			<VariablePicker
+				variable={variable}
+				onChange={setVariable}
+				showLabel={false}
+				variables={variables}
+			/>
+		)
+	}, [variable, variables, setVariable])
+}
+
+export const useDescriptionBox = (
+	description: string,
+	setDescription: GenericFn,
+	variable: string,
+	add: GenericFn,
+	factor?: Factor,
+): JSX.Element => {
+	return useMemo(() => {
+		return (
+			<DetailsContainer>
+				<Field
+					onChange={(_, value) => setDescription(value)}
+					value={description}
+					placeholder="Enter description"
+					multiline={(description?.length || 0) > 70}
+					resizable={false}
+				/>
+				{!factor ? (
+					<ButtonContainer>
+						<AddButton disabled={!variable?.length} onClick={add}>
+							Add
+						</AddButton>
+					</ButtonContainer>
+				) : null}
+			</DetailsContainer>
+		)
+	}, [description, setDescription, variable, add, factor])
+}
+
+export const useHasLevel = (factor?: Factor): boolean => {
+	return useMemo(() => !!factor?.hasOwnProperty('level'), [factor])
+}
+
+const DetailsContainer = styled.div`
+	display: flex;
+`
+
+const Field = styled(TextField)`
+	width: 100%;
+	margin: 0px 8px;
+`
+
+const AddButton = styled(DefaultButton)``
+
+const ButtonContainer = styled.div`
+	text-align: end;
+	padding-right: 8px;
+`
