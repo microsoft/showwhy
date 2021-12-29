979b9ef933a4021f9edd7e9da95f141b647d6947
Dayenne Souza <ddesouza@microsoft.com>
2021-12-29 11:44:50 -0300

    fix lint

--- packages/client/src/classes/orchestrator.ts
+++ packages/client/src/classes/orchestrator.ts
@@ -57,7 +57,7 @@ export class Orchestrator {
 
 		let estimateStatus
 		while (isStatusProcessing(status?.runtimeStatus as NodeResponseStatus)) {
-			;[status, estimateStatus] = await Promise.all([
+			[status, estimateStatus] = await Promise.all([
 				returnOrchestratorStatus(this.orchestratorResponse.statusQueryGetUri),
 				genericCheckStatus(status?.instanceId, type),
 				wait(3000),
