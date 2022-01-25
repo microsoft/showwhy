9cb9a9f6167cf744782b4e327cafa2512a3d29f6
Chris Trevino <chtrevin@microsoft.com>
2022-01-25 15:18:10 -0800

    move visualization types to folder

--- packages/client/src/types/visualization/Specification.ts
+++ packages/client/src/types/visualization/Specification.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 
-import { RefutationResult } from './refutation'
+import { RefutationResult } from '../refutation'
 
 export interface Specification {
 	id: number
