e14f98312607bc81b3efcc60e71c45b865d67f2a
Chris Trevino <chtrevin@microsoft.com>
2022-02-15 11:16:33 -0800

    remove unused guidance state export

--- packages/client/src/state/__tests__/guidance.spec.ts
+++ packages/client/src/state/__tests__/guidance.spec.ts
@@ -3,27 +3,17 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import { renderHook } from '@testing-library/react-hooks'
-import { RecoilRoot, snapshot_UNSTABLE } from 'recoil'
-import {guidanceState, useGuidance } from '../guidance'
+import { RecoilRoot } from 'recoil'
+import { useGuidance } from '../guidance'
 
 describe('guidanceState', () => {
-
-    describe('useGuidance', () => {
-        it('should return true as default value', () => {
-            const expected = true
-            const { result } = renderHook(() => useGuidance(), {
-                wrapper: RecoilRoot,
-            })
-            expect(result.current).toEqual(expected)
-        })
-    })
-
-    describe('useSetGuidance', () => {
-        it('should set the guidance to false', () => {
-            const expected = false
-            const snapshot = snapshot_UNSTABLE(({set}) => set(guidanceState, expected)).getLoadable(guidanceState).valueOrThrow() 
-            expect(snapshot).toBe(expected)
-        })
-    })
-
+	describe('useGuidance', () => {
+		it('should return true as default value', () => {
+			const expected = true
+			const { result } = renderHook(() => useGuidance(), {
+				wrapper: RecoilRoot,
+			})
+			expect(result.current).toEqual(expected)
+		})
+	})
 })
--- packages/client/src/state/guidance.ts
+++ packages/client/src/state/guidance.ts
@@ -2,13 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import {
-	atom,
-	SetterOrUpdater,
-	useRecoilValue,
-	useSetRecoilState,
-	useRecoilState,
-} from 'recoil'
+import { atom, useRecoilValue, useRecoilState } from 'recoil'
 
 export const guidanceState = atom<boolean>({
 	key: 'guidance',
@@ -21,8 +15,4 @@ export function useGuidanceState() {
 
 export function useGuidance(): boolean {
 	return useRecoilValue(guidanceState)
-}
-
-export function useSetGuidance(): SetterOrUpdater<boolean> {
-	return useSetRecoilState(guidanceState)
 }
