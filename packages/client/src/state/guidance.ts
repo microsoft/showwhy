bfae7016af2a0afc781732f4a02ac15366bb1854
Chris Trevino <chtrevin@microsoft.com>
2022-02-15 12:05:32 -0800

    refactor the guidance localStorageEffect into a reusable effect fn

--- packages/client/src/state/guidance.ts
+++ packages/client/src/state/guidance.ts
@@ -3,29 +3,12 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import { atom, useRecoilValue, useRecoilState, SetterOrUpdater } from 'recoil'
-
-const SHOW_GUIDANCE_LS_KEY = 'showwhy:show_guidance'
-
-function defaultShowGuidance(): boolean {
-	const found = localStorage.getItem(SHOW_GUIDANCE_LS_KEY)
-	if (found != null) {
-		try {
-			return JSON.parse(found)
-		} catch (err: unknown) {}
-	}
-	return true
-}
+import { localStorageEffect } from './effects/localStorageEffect'
 
 export const guidanceState = atom<boolean>({
 	key: 'guidance',
-	default: defaultShowGuidance(),
-	effects: [
-		({ onSet }) => {
-			onSet(newVal => {
-				localStorage.setItem(SHOW_GUIDANCE_LS_KEY, JSON.stringify(newVal))
-			})
-		},
-	],
+	default: true,
+	effects: [localStorageEffect('showwhy:show_guidance')],
 })
 
 export function useGuidanceState(): [boolean, SetterOrUpdater<boolean>] {
