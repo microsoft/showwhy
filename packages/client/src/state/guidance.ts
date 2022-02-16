abaa89c7fce9d0830d5bacb7d725d235bc1dc701
Chris Trevino <chtrevin@microsoft.com>
2022-02-16 12:27:35 -0800

    add common hook for boolean toggle-state based in recoil

--- packages/client/src/state/guidance.ts
+++ packages/client/src/state/guidance.ts
@@ -2,8 +2,9 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { atom, useRecoilValue, useRecoilState, SetterOrUpdater } from 'recoil'
+import { atom } from 'recoil'
 import { localStorageEffect } from './effects/localStorageEffect'
+import { ToggleCallback, useRecoilBasedToggle } from '~hooks'
 
 export const guidanceState = atom<boolean>({
 	key: 'guidance',
@@ -11,10 +12,6 @@ export const guidanceState = atom<boolean>({
 	effects: [localStorageEffect('showwhy:show_guidance')],
 })
 
-export function useGuidanceState(): [boolean, SetterOrUpdater<boolean>] {
-	return useRecoilState(guidanceState)
-}
-
-export function useGuidance(): boolean {
-	return useRecoilValue(guidanceState)
+export function useGuidance(): [boolean, ToggleCallback] {
+	return useRecoilBasedToggle(guidanceState)
 }
