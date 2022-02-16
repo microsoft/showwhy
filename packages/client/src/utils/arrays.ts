f9ae6be1eac7abb88a6003a75b7e73cfeda1b883
Chris Trevino <chtrevin@microsoft.com>
2022-02-16 13:47:41 -0800

    remove unused export

--- packages/client/src/utils/arrays.ts
+++ packages/client/src/utils/arrays.ts
@@ -17,10 +17,3 @@ export function addOrRemoveArrayElement(
 ): string[] {
 	return add ? [...array, item] : array.filter(d => d !== item)
 }
-
-export function equalArrays(arr1: any[], arr2: any[]): boolean {
-	if (arr1.length !== arr2.length) {
-		return false
-	}
-	return arr1.every(item => arr2.includes(item))
-}
