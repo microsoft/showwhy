b20883e4c69c8c061db95974765c614f1e9a01af
Andres Morales Esquivel <andresmor@microsoft.com>
2022-02-25 09:32:37 -0600

    Backend refactor into libs

---
+++ python/showwhy-backend/showwhy_backend/GetNumberOfExecutions/__init__.py
@@ -0,0 +1,27 @@
+#
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project.
+#
+
+import json
+
+import azure.functions as func
+from showwhy_inference.inference import generate_all_specs
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    body = json.loads(req.get_body())
+    node_data = body['node_data']
+
+    specs = list(generate_all_specs(node_data['population_specs'],
+                                    node_data['treatment_specs'],
+                                    node_data['outcome_specs'],
+                                    node_data['model_specs'],
+                                    node_data['estimator_specs']))
+
+    return func.HttpResponse(
+        body=json.dumps({
+            'total_executions': len(specs)
+        })
+    )
---
+++ python/showwhy-backend/showwhy_backend/RefuteActivity/__init__.py
@@ -0,0 +1,52 @@
+#
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project.
+#
+
+import logging
+from typing import Dict
+
+from showwhy_inference.refutation import Refutation
+from shared_code.io.storage import get_storage_client
+from shared_code.io.db import get_db_client
+
+storage = get_storage_client()
+db = get_db_client()
+
+
+def main(body: Dict) -> str:
+    input_file = body['input_file']
+    session_id = body['session_id']
+    refuter_spec = body['spec']
+    execution_id = body['execution_id']
+    task_id = body['task_id']
+
+    logging.info(
+        f"Executing spec: [{refuter_spec}] for session: [{session_id}]")
+
+    additional_inputs = storage.read_output(
+        session_id, input_file, file_type='partial')
+
+    estimated_effect = additional_inputs['estimated_effect']
+
+    if estimated_effect:
+        causal_model, identified_estimand, estimate = estimated_effect
+        refuter_result = Refutation(refuter_spec).refute_estimate(
+            causal_model, identified_estimand, estimate)
+
+        result = {
+            **additional_inputs,
+            **refuter_result,
+            'task_id': task_id,
+            'estimated_effect': estimate.value if estimate else None
+        }
+
+        db.insert_record({
+            **refuter_result,
+            'execution_id': execution_id,
+            'task_id': f'{task_id}'
+        })
+
+        return result
+    else:
+        return {}
