791d03646e37783d8af2ba7d5150d8afe6befe5b
Chris Trevino <chtrevin@microsoft.com>
2022-03-02 09:12:46 -0800

    correct some types imports

--- javascript/client/src/types/dataTables/index.ts
+++ javascript/client/src/types/dataTables/index.ts
@@ -3,3 +3,8 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 export * from './DataTable' //VERIFY
+export * from './TableColumn'
+export * from './TableDerivation'
+export * from './TableDerivationType'
+export * from './ColumnRelation'
+export * from './ColumnRelevance'
