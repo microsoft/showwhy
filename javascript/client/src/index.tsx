be04c7545f3d73852eaf5d5b89f46d3eba0968fa
Dayenne Souza <day.carol18@gmail.com>
2022-03-18 17:17:23 -0300

    update initializeIcons()

--- javascript/client/src/index.tsx
+++ javascript/client/src/index.tsx
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+import { initializeIcons } from '@fluentui/font-icons-mdl2'
 import { render } from 'react-dom'
 
 import { App } from './components'
@@ -14,4 +15,5 @@ function mount(): void {
 		console.error('error rendering application', err)
 	}
 }
+initializeIcons()
 mount()
