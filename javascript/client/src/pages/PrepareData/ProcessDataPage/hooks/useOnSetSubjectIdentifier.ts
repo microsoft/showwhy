775ffadfcd2619895da9b1701dddbbfe2b9e8ad8
Dayenne <ddesouza@microsoft.com>
2022-04-07 22:08:56 -0300

    prepare data automatic

--- javascript/client/src/pages/PrepareData/ProcessDataPage/hooks/useOnSetSubjectIdentifier.ts
+++ javascript/client/src/pages/PrepareData/ProcessDataPage/hooks/useOnSetSubjectIdentifier.ts
@@ -9,15 +9,18 @@ import { useCallback } from 'react'
 export function useOnSetSubjectIdentifier(
 	subjectIdentifier: Maybe<string>,
 	setSubjectIdentifier: Handler1<Maybe<string>>,
+	onSelect: Handler1<boolean>,
 ): Handler1<Maybe<string>> {
 	return useCallback(
 		(columnName: string | undefined) => {
 			if (subjectIdentifier === columnName) {
 				setSubjectIdentifier(undefined)
+				onSelect(false)
 			} else {
 				setSubjectIdentifier(columnName)
+				onSelect(true)
 			}
 		},
-		[subjectIdentifier, setSubjectIdentifier],
+		[subjectIdentifier, setSubjectIdentifier, onSelect],
 	)
 }
