5a5a6b7f769e68194df2ade69cfdd390a94a1086
Monica Carvajal <mcarvajal@microsoft.com>
2022-08-23 10:20:48 -0500

    updated package name

--- javascript/webapp/src/pages/PrepareData/DeriveDataVariablesPage/components/PrepareData.tsx
+++ javascript/webapp/src/pages/PrepareData/DeriveDataVariablesPage/components/PrepareData.tsx
@@ -2,8 +2,8 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+import type { TableContainer } from '@datashaper/arquero'
 import { PrepareDataFull } from '@datashaper/react'
-import type { TableContainer } from '@essex/arquero'
 import type { IDetailsColumnProps, IRenderFunction } from '@fluentui/react'
 import type { FC } from 'react'
 import { memo, useEffect, useMemo, useState } from 'react'
--- javascript/webapp/src/state/outputTables.ts
+++ javascript/webapp/src/state/outputTables.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import type { TableContainer } from '@essex/arquero'
+import type { TableContainer } from '@datashaper/arquero'
 import type { Resetter, SetterOrUpdater } from 'recoil'
 import {
 	atom,
