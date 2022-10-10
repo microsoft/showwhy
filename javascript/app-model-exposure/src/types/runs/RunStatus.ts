6f4f767f062172c8e3dfbf8d32a635702af94b8e
Chris Trevino <darthtrevino@gmail.com>
2022-10-10 12:10:47 -0700

    add missing files

---
+++ javascript/app-model-exposure/src/types/runs/RunStatus.ts
@@ -0,0 +1,23 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+
+import type { NodeResponseStatus } from '../api/NodeResponseStatus.js'
+
+interface RunTime {
+	start: Date
+	end?: Date
+}
+
+export interface RunStatus {
+	percentage: number
+	estimated_effect_completed: number
+	refute_completed: number
+	confidence_interval_completed: number
+	confidence_interval_pending: number
+	shap_completed: number
+	status: NodeResponseStatus
+	time?: RunTime
+	error?: string
+}
