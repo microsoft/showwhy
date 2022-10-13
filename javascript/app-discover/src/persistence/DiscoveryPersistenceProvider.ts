90d3441c4312c34e39af8dc8e961a3e5cae81a86
Chris Trevino <darthtrevino@gmail.com>
2022-10-13 16:25:46 -0700

    persist discover app atoms

--- javascript/app-discover/src/persistence/DiscoveryPersistenceProvider.ts
+++ javascript/app-discover/src/persistence/DiscoveryPersistenceProvider.ts
@@ -3,9 +3,64 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import { AppResourceHandler, useDataPackage } from '@showwhy/app-common'
-import { memo, useEffect, useMemo } from 'react'
+import { memo, useCallback, useEffect, useMemo } from 'react'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 
-type ProjectJson = any
+import type { GraphViewStates } from '../components/graph/GraphViews.types.js'
+import type { CausalDiscoveryAlgorithm } from '../domain/CausalDiscovery/CausalDiscoveryAlgorithm.js'
+import type { CausalDiscoveryConstraints } from '../domain/CausalDiscovery/CausalDiscoveryConstraints.js'
+import type { CausalDiscoveryResult } from '../domain/CausalDiscovery/CausalDiscoveryResult.js'
+import type { Intervention } from '../domain/CausalInference.js'
+import type { NodePosition } from '../domain/NodePosition.js'
+import {
+	AutoLayoutEnabledState,
+	CausalDiscoveryResultsState,
+	CausalGraphConstraintsState,
+	CausalInferenceBaselineOffsetsState,
+	CausalInferenceBaselineValuesState,
+	CausalInferenceResultState,
+	CausalInterventionsState,
+	ConfidenceThresholdState,
+	CorrelationThresholdState,
+	DatasetNameState,
+	GraphViewState,
+	InModelColumnNamesState,
+	NodePositionsState,
+	SelectedCausalDiscoveryAlgorithmState,
+	ShowChangesInGraphState,
+	StraightEdgesState,
+	WeightThresholdState,
+} from '../state/index.js'
+
+interface ProjectJson {
+	causalGraph: {
+		inModelColumnNames: string[]
+		constraints: CausalDiscoveryConstraints
+		results: CausalDiscoveryResult
+	}
+	causalInference: {
+		interventions: Intervention[]
+		baselineValues: Map<string, number>
+		baselineOffsets: Map<string, number>
+		results: Map<string, number>
+	}
+	dataset: {
+		datasetName: string
+	}
+	layout: {
+		nodePositions: { [key: string]: NodePosition }
+	}
+	ui: {
+		straightEdges: boolean
+		autoLayoutEnabled: boolean
+		showChangesInGraph: boolean
+		selectedDiscoveryAlgorithm: CausalDiscoveryAlgorithm
+		weightThreshold: number
+		confidenceThreshold: number
+		correlationThreshold: number
+		view: GraphViewStates
+	}
+}
 
 export const DiscoveryPersistenceProvider: React.FC = memo(
 	function ModelExposurePersistenceProvider() {
@@ -34,11 +89,145 @@ export const DiscoveryPersistenceProvider: React.FC = memo(
 )
 
 function useGetProjectJson(): () => ProjectJson {
-	return () => ({})
+	const inModelColumnNames = useRecoilValue(InModelColumnNamesState)
+	const causalGraphConstraints = useRecoilValue(CausalGraphConstraintsState)
+	const causalDiscoveryResultsState = useRecoilValue(
+		CausalDiscoveryResultsState,
+	)
+	const causalInterventions = useRecoilValue(CausalInterventionsState)
+	const baselineValues = useRecoilValue(CausalInferenceBaselineValuesState)
+	const baselineOffsets = useRecoilValue(CausalInferenceBaselineOffsetsState)
+	const inferenceResults = useRecoilValue(CausalInferenceResultState)
+	const datasetName = useRecoilValue(DatasetNameState)
+	const nodePositions = useRecoilValue(NodePositionsState)
+	const straightEdges = useRecoilValue(StraightEdgesState)
+	const autoLayoutEnabled = useRecoilValue(AutoLayoutEnabledState)
+	const showChangesInGraph = useRecoilValue(ShowChangesInGraphState)
+	const selectedDiscoveryAlgorithm = useRecoilValue(
+		SelectedCausalDiscoveryAlgorithmState,
+	)
+	const weightThreshold = useRecoilValue(WeightThresholdState)
+	const confidenceThreshold = useRecoilValue(ConfidenceThresholdState)
+	const correlationThreshold = useRecoilValue(CorrelationThresholdState)
+	const view = useRecoilValue(GraphViewState)
+
+	return useCallback(
+		() => ({
+			causalGraph: {
+				inModelColumnNames,
+				constraints: causalGraphConstraints,
+				results: causalDiscoveryResultsState,
+			},
+			causalInference: {
+				interventions: causalInterventions,
+				baselineValues,
+				baselineOffsets,
+				results: inferenceResults,
+			},
+			dataset: {
+				datasetName,
+			},
+			layout: { nodePositions },
+			ui: {
+				straightEdges,
+				autoLayoutEnabled,
+				showChangesInGraph,
+				selectedDiscoveryAlgorithm,
+				weightThreshold,
+				confidenceThreshold,
+				correlationThreshold,
+				view,
+			},
+		}),
+		[
+			inModelColumnNames,
+			causalGraphConstraints,
+			causalDiscoveryResultsState,
+			causalInterventions,
+			baselineValues,
+			baselineOffsets,
+			inferenceResults,
+			datasetName,
+			nodePositions,
+			straightEdges,
+			autoLayoutEnabled,
+			showChangesInGraph,
+			selectedDiscoveryAlgorithm,
+			weightThreshold,
+			confidenceThreshold,
+			correlationThreshold,
+			view,
+		],
+	)
 }
 
 function useLoadProjectJson(): (json: ProjectJson) => void {
-	return (json: ProjectJson) => {
-		console.log('TODO: load json', json)
-	}
+	const setInModelColumnNames = useSetRecoilState(InModelColumnNamesState)
+	const setCausalGraphConstraints = useSetRecoilState(
+		CausalGraphConstraintsState,
+	)
+	const setCausalDiscoveryResultsState = useSetRecoilState(
+		CausalDiscoveryResultsState,
+	)
+	const setCausalInterventions = useSetRecoilState(CausalInterventionsState)
+	const setBaselineValues = useSetRecoilState(
+		CausalInferenceBaselineValuesState,
+	)
+	const setBaselineOffsets = useSetRecoilState(
+		CausalInferenceBaselineOffsetsState,
+	)
+	const setInferenceResults = useSetRecoilState(CausalInferenceResultState)
+	const setDatasetName = useSetRecoilState(DatasetNameState)
+	const setNodePositions = useSetRecoilState(NodePositionsState)
+	const setStraightEdges = useSetRecoilState(StraightEdgesState)
+	const setAutoLayoutEnabled = useSetRecoilState(AutoLayoutEnabledState)
+	const setShowChangesInGraph = useSetRecoilState(ShowChangesInGraphState)
+	const setSelectedDiscoveryAlgorithm = useSetRecoilState(
+		SelectedCausalDiscoveryAlgorithmState,
+	)
+	const setWeightThreshold = useSetRecoilState(WeightThresholdState)
+	const setConfidenceThreshold = useSetRecoilState(ConfidenceThresholdState)
+	const setCorrelationThreshold = useSetRecoilState(CorrelationThresholdState)
+	const setView = useSetRecoilState(GraphViewState)
+
+	return useCallback(
+		(json: ProjectJson) => {
+			setInModelColumnNames(json.causalGraph.inModelColumnNames)
+			setCausalGraphConstraints(json.causalGraph.constraints)
+			setCausalDiscoveryResultsState(json.causalGraph.results)
+			setCausalInterventions(json.causalInference.interventions)
+			setBaselineValues(json.causalInference.baselineValues)
+			setBaselineOffsets(json.causalInference.baselineOffsets)
+			setInferenceResults(json.causalInference.results)
+			setDatasetName(json.dataset.datasetName)
+			setNodePositions(json.layout.nodePositions)
+			setStraightEdges(json.ui.straightEdges)
+			setAutoLayoutEnabled(json.ui.autoLayoutEnabled)
+			setShowChangesInGraph(json.ui.showChangesInGraph)
+			setSelectedDiscoveryAlgorithm(json.ui.selectedDiscoveryAlgorithm)
+			setWeightThreshold(json.ui.weightThreshold)
+			setConfidenceThreshold(json.ui.confidenceThreshold)
+			setCorrelationThreshold(json.ui.correlationThreshold)
+			setView(json.ui.view)
+		},
+		[
+			setInModelColumnNames,
+			setCausalGraphConstraints,
+			setCausalDiscoveryResultsState,
+			setCausalInterventions,
+			setBaselineValues,
+			setBaselineOffsets,
+			setInferenceResults,
+			setDatasetName,
+			setNodePositions,
+			setStraightEdges,
+			setAutoLayoutEnabled,
+			setShowChangesInGraph,
+			setSelectedDiscoveryAlgorithm,
+			setWeightThreshold,
+			setConfidenceThreshold,
+			setCorrelationThreshold,
+			setView,
+		],
+	)
 }
