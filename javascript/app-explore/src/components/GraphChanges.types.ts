1d0a19d6b987da7c0a1533afea94660c3483a408
Chris Trevino <darthtrevino@gmail.com>
2022-10-13 12:10:30 -0700

    remove graph-changes panel

--- javascript/app-explore/src/components/GraphChanges.types.ts
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft. All rights reserved.
- * Licensed under the MIT license. See LICENSE file in the project.
- */
-import type { GraphDifferences } from '../domain/GraphDifferences.js'
-
-export interface GraphDifferenceListProps {
-	differences: GraphDifferences
-}
--- javascript/app-explore/src/components/panels/GraphPropertiesPanel.tsx
+++ javascript/app-explore/src/components/panels/GraphPropertiesPanel.tsx
@@ -3,29 +3,19 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import { memo } from 'react'
-import { useRecoilValue } from 'recoil'
 
-import { CausalGraphChangesState } from '../../state/index.js'
 import { AlgorithmControls } from '../AlgorithmControls.js'
 import { Divider } from '../controls/Divider.js'
-import { GraphDifferenceList } from '../GraphChanges.js'
 import { ConstraintsPanel } from './ConstraintsPanel.js'
 
 export const GraphPropertiesPanel: React.FC = memo(
 	function GraphPropertiesPanel() {
-		const changes = useRecoilValue(CausalGraphChangesState)
 		return (
 			<>
 				<AlgorithmControls />
 				<ConstraintsPanel>
 					<Divider>Constraints</Divider>
 				</ConstraintsPanel>
-				{changes && (
-					<>
-						<Divider>Changes</Divider>
-						<GraphDifferenceList differences={changes}></GraphDifferenceList>
-					</>
-				)}
 			</>
 		)
 	},
