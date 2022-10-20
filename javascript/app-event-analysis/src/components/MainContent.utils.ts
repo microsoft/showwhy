52ac119835f72ee56b913b26a940f6e08f5c935b
Dayenne de Souza <ddesouza@microsoft.com>
2022-10-20 12:01:05 -0300

    fix bug with string array

--- javascript/app-event-analysis/src/components/MainContent.utils.ts
+++ javascript/app-event-analysis/src/components/MainContent.utils.ts
@@ -59,7 +59,11 @@ export function processSynthControlData(
 
 		if (outputData.weighted_synthdid_controls) {
 			const scWeights = outputData.weighted_synthdid_controls.weights
-			const scUnits = outputData.weighted_synthdid_controls.dimnames
+			let scUnits = outputData.weighted_synthdid_controls.dimnames
+			// When there's only one unit returning, the API returns it as a string and we're expecting an array of strings
+			if (typeof scUnits === 'string') {
+				scUnits = [scUnits]
+			}
 			scUnits.forEach((unit, indx) => {
 				if (checkedUnits !== null && checkedUnits.has(unit)) {
 					const weight = scWeights[indx]
