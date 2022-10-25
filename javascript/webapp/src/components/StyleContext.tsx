c8ed858b12fb3f963ef33e02e96a192dddecb318
Nathan Evans <github@talkswithnumbers.com>
2022-10-25 15:20:49 -0700

    Revert to default theme

--- javascript/webapp/src/components/StyleContext.tsx
+++ javascript/webapp/src/components/StyleContext.tsx
@@ -2,8 +2,8 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import type { SwatchRGB } from '@fluentui/web-components';
-import { accentBaseColor,controlCornerRadius  } from '@fluentui/web-components'
+import type { SwatchRGB } from '@fluentui/web-components'
+import { accentBaseColor, controlCornerRadius } from '@fluentui/web-components'
 import { loadById } from '@thematic/core'
 import { loadFluentTheme, ThematicFluentProvider } from '@thematic/fluent'
 import { ApplicationStyles, useThematic } from '@thematic/react'
@@ -15,7 +15,7 @@ import type { StyleContextProps } from './StyleContext.types.js'
 
 export const StyleContext: React.FC<StyleContextProps> = memo(
 	function StyleContext({ children }) {
-		const theme = loadById('autumn', { dark: false })
+		const theme = loadById('default', { dark: false })
 		const fluentTheme = useMemo(() => loadFluentTheme(theme), [theme])
 		return (
 			<ThematicFluentProvider theme={theme} style={fluentProviderStyle}>
