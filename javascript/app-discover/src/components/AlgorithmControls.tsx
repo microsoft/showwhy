4eab4d6f995e7c7f232d0101101c27af467212cb
Nathan Evans <github@talkswithnumbers.com>
2022-10-25 12:01:11 -0700

    Fix algo pane padding

--- javascript/app-discover/src/components/AlgorithmControls.tsx
+++ javascript/app-discover/src/components/AlgorithmControls.tsx
@@ -6,6 +6,7 @@ import type { IChoiceGroupOption } from '@fluentui/react'
 import { ChoiceGroup } from '@fluentui/react'
 import { memo, useCallback } from 'react'
 import { useRecoilState } from 'recoil'
+import styled from 'styled-components'
 
 import { CausalDiscoveryAlgorithm } from '../domain/CausalDiscovery/CausalDiscoveryAlgorithm.js'
 import { SelectedCausalDiscoveryAlgorithmState } from '../state/index.js'
@@ -32,7 +33,7 @@ export const AlgorithmControls = memo(function AlgorithmControls() {
 	)
 
 	return (
-		<>
+		<Container>
 			<Divider>Discovery algorithm</Divider>
 			<ChoiceGroup
 				selectedKey={selectedCausalDiscoveryAlgorithm}
@@ -42,6 +43,10 @@ export const AlgorithmControls = memo(function AlgorithmControls() {
 			{selectedCausalDiscoveryAlgorithm === CausalDiscoveryAlgorithm.DECI && (
 				<DeciParams />
 			)}
-		</>
+		</Container>
 	)
 })
+
+const Container = styled.div`
+	padding: 8px;
+`
