6d3912dcfc2b8182e4b16d87b9779c9f115fb8c3
Rodrigo Racanicci <rracanicci@microsoft.com>
2022-11-04 16:21:25 -0300

    allow number of concurrent jobs per worker to be controlled in the backend

--- python/backend/scripts/start_worker.sh
+++ python/backend/scripts/start_worker.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+if [[ -z "$N_PARALLEL_JOBS" ]]; then
+    extra_params=""
+else
+    extra_params="--concurrency $N_PARALLEL_JOBS"
+fi
+
 if [[ -z "${DEBUG}" ]]; then
     # worker debug disabled
-    echo "Starting backend worker with debug disabled"
-    poetry run python -m celery -A backend.worker_main worker -l info
+    echo "Starting backend worker with debug disabled (extra_params=$extra_params)"
+    poetry run python -m celery -A backend.worker_main worker -l info $extra_params
 else
     # worker debug enabled
-    echo "Starting backend worker with debug enabled"
-    poetry run python -m debugpy --listen 0.0.0.0:6901 -m celery -A backend.worker_main worker -l info
+    echo "Starting backend worker with debug enabled (extra_params=$extra_params)"
+    poetry run python -m debugpy --listen 0.0.0.0:6901 -m celery -A backend.worker_main worker -l info $extra_params
 fi
