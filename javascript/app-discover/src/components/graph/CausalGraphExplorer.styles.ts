4f1063caa854a2e03123b4c2009019eafbc01853
Dayenne de Souza <ddesouza@microsoft.com>
2022-11-07 18:45:14 -0300

    add all legends

--- javascript/app-discover/src/components/graph/CausalGraphExplorer.styles.ts
+++ javascript/app-discover/src/components/graph/CausalGraphExplorer.styles.ts
@@ -4,9 +4,11 @@
  */
 import { mergeStyleSets } from '@fluentui/react'
 import styled from 'styled-components'
+
 import {
 	colorCorrelation,
 	colorNegative,
+	colorNeutral,
 	colorPositive,
 } from '../../styles/styles.js'
 
@@ -16,7 +18,27 @@ export const Background = styled.div({
 	minWidth: '100%',
 })
 
-export const Container = styled.div``
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+`
+export const Grid = styled.div`
+	display: grid;
+	grid-auto-columns: 1fr 1fr 1fr;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	gap: 5px 5px;
+	grid-template-areas:
+		'. . . .'
+		'. . edge edge';
+	justify-content: space-between;
+	align-content: space-around;
+	justify-items: stretch;
+	align-items: stretch;
+`
+
+export const ContainerEdge = styled(Container)`
+	grid-area: edge;
+`
 
 export const FlexContainer = styled.div`
 	height: 98%;
@@ -30,4 +52,5 @@ export const classNames = mergeStyleSets({
 	positive: [{ color: colorPositive }],
 	negative: [{ color: colorNegative }],
 	correlation: [{ color: colorCorrelation }],
+	pcChange: [{ color: colorNeutral }],
 })
