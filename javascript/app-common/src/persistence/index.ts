93327c99c46c35b8985e29ce5c28dd7ecda66c3f
Chris Trevino <darthtrevino@gmail.com>
2022-11-10 16:01:22 -0800

    delete deplicate components

--- javascript/app-common/src/persistence/index.ts
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft. All rights reserved.
- * Licensed under the MIT license. See LICENSE file in the project.
- */
-export * from './AppResourceHandler.js'
-export * from './DefaultPersistenceService.js'
-export * from './PersistenceContext.js'
-export * from './PersistenceProvider.js'
-export * from './types.js'
--- javascript/app-wrangle/src/components/TableEditor.types.ts
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft. All rights reserved.
- * Licensed under the MIT license. See LICENSE file in the project.
- */
-import type { DataTable } from '@datashaper/workflow'
-
-export interface TableEditorProps {
-	dataTable: DataTable
-}
