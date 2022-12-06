5509fac44e70adb33d5975c12e5c3e86e082bb57
Dayenne de Souza <ddesouza@microsoft.com>
2022-12-06 19:39:22 -0300

    helm

--- helm.sh
+++ helm.sh
@@ -6,7 +6,7 @@ wget -O helm.tgz https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
 #tar -zxvf helm.tgz
 #mv linux-amd64/helm /usr/local/bin/helm
 # Install kubectl
-az aks install-cli
+#az aks install-cli
 echo "LOGIN..."
 # Get cluster credentials
 az aks get-credentials -g $RESOURCEGROUP -n $CLUSTER_NAME
