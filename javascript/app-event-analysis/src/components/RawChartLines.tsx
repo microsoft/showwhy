dc9cb8eb5c5dd48daf8d52f0e47044185f6f916d
Monica Carvajal <mcarvajal@microsoft.com>
2022-12-07 00:02:31 -0500

    raw chart lines component

---
+++ javascript/app-event-analysis/src/components/RawChartLines.tsx
@@ -0,0 +1,43 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import React, { memo } from 'react'
+
+import { useTreatedUnitsMap } from '../hooks/useTreatedUnitsMap.js'
+import { Line } from './Line.js'
+import { RawChartLinesProps } from './RawChartLines.types.js'
+import { useLinePropsGetters } from './RawChartLines.hooks.js'
+
+export const RawChartLines: React.FC<RawChartLinesProps> = memo(
+	function RawChartLines({
+		xScale,
+		yScale,
+		treatedUnits,
+		lineChartData,
+		mouseHandlers,
+	}) {
+		const treatedUnitsMap = useTreatedUnitsMap(treatedUnits)
+		const { handleLineMouseMove, handleLineMouseLeave, handleLineMouseClick } =
+			mouseHandlers
+		const { getClassName, getOpacity, getStrokeWidth, getColor } =
+			useLinePropsGetters(treatedUnitsMap)
+		const { inputLines } = lineChartData
+
+		return inputLines.map((ld, index) => (
+			<Line
+				key={index}
+				className={getClassName(ld)}
+				color={getColor(ld)}
+				xScale={xScale}
+				yScale={yScale}
+				data={ld}
+				opacity={getOpacity(ld)}
+				strokeWidth={getStrokeWidth(ld)}
+				onMouseMove={handleLineMouseMove}
+				onMouseLeave={handleLineMouseLeave}
+				onClick={handleLineMouseClick}
+			/>
+		))
+	},
+)
