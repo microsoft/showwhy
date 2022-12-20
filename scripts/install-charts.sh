cfd1d4d141a62bfc00654f7e2326189a0df26b9f
Chris Trevino <darthtrevino@gmail.com>
2022-12-20 11:45:01 -0800

    localdev -> local

--- scripts/install-charts.sh
+++ scripts/install-charts.sh
@@ -7,7 +7,7 @@ helm package config/helm/causal-services --destination config/helm/dist
 
 if [ -z "$values_file" ]; then
     echo "Installing causal-services with local values..."
-    helm upgrade --install causal-services config/helm/dist/causal-services-$backend_version.tgz -f config/helm/causal-services/values.localdev.yaml
+    helm upgrade --install causal-services config/helm/dist/causal-services-$backend_version.tgz -f config/helm/causal-services/values.local.yaml
 else
     echo "Installing causal-services with values from file '$values_file'..."
     helm upgrade --install causal-services config/helm/dist/causal-services-$backend_version.tgz -f $values_file --wait
