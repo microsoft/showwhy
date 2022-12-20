c863453a16d8e92c5608c35a44192ca1ea3eb02a
Dayenne de Souza <ddesouza@microsoft.com>
2022-12-20 19:16:49 -0300

    update aad

--- aad-aks.sh
+++ aad-aks.sh
@@ -3,38 +3,46 @@ set -e
 
 az account set --subscription $SUBSCRIPTION
 az aks install-cli
+
 # Get cluster credentials
 az aks get-credentials -g $RESOURCEGROUP -n $CLUSTER_NAME
 echo "LOGIN SUCCESSFUL"
 
 secretName=Default
-# az extension add --upgrade -n account
-
-# tenantId=`az account tenant list --query [0].tenantId`
 
 # add graph api email and openid access (universally known ids)
 cat > app-manifest.json << EOF
-[
-    {
-        "resourceAppId": "00000003-0000-0000-c000-000000000000",
-			"resourceAccess": [
-				{
-					"id": "37f7f235-527c-4136-accd-4a02d197296e",
-					"type": "Scope"
-				},
-				{
-					"id": "64a6cdd6-aab1-4aaf-94b8-3cc8405e90d0",
-					"type": "Scope"
-				}
-			]
-        "accessTokenAcceptedVersion": 2,
-        "signInAudience": "AzureADMyOrg"
-    }
-]
+{
+    "requiredResourceAccess": [
+        {
+            "resourceAppId": "00000003-0000-0000-c000-000000000000",
+            "resourceAccess": [
+                {
+                    "id": "37f7f235-527c-4136-accd-4a02d197296e",
+                    "type": "Scope"
+                },
+                {
+                    "id": "64a6cdd6-aab1-4aaf-94b8-3cc8405e90d0",
+                    "type": "Scope"
+                }
+                ],
+            
+        }
+    ],
+    "signInAudience": "AzureADMyOrg",
+    "logoutUrl": "https://$DNS_NAME.$LOCATION.cloudapp.azure.com/oauth2/sign_out",
+    "replyUrlsWithType": [
+        {
+            "url": "https://$DNS_NAME.$LOCATION.cloudapp.azure.com/oauth2/callback",
+            "type": "Web"
+        }
+    ],
+    "identifierUris": ["api://$DNS_NAME"]
+}
 EOF
 
 # Create app registration
-appId=`az ad app create --display-name $CLUSTER_NAME --identifier-uris "api://$CLUSTER_NAME" --web-redirect-uris https://$DNS_PREFIX.$LOCATION.cloudapp.azure.com/oauth2/callback --required-resource-accesses @app-manifest.json --query id -o tsv`
+appId=`az ad app create --display-name $CLUSTER_NAME --required-resource-accesses @app-manifest.json --query id -o tsv`
 
 # Get client secret
 appSecret=`az ad app credential reset --id $appId --display-name $secretName --query password --o tsv`
