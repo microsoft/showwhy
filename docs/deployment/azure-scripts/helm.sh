dbcdf1efa5e690c84b8b7445813fac25a0e8bc73
Dayenne de Souza <ddesouza@microsoft.com>
2022-12-22 14:05:40 -0300

    add optional auth

--- docs/deployment/azure-scripts/helm.sh
+++ docs/deployment/azure-scripts/helm.sh
@@ -12,9 +12,36 @@ az aks install-cli
 # Get cluster credentials
 az aks get-credentials -g $RESOURCEGROUP -n $CLUSTER_NAME
 
+auth=false
+if [ -v $CLIENT_ID ];then
+    auth=true
+    cookieName=_auth_token
+
+    randomSecretCookie=`python -c 'import os,base64; print(base64.urlsafe_b64encode(os.urandom(32)).decode())'`
+
+    # kubectl create namespace oauth-proxy
+
+    cat > oauth-proxy.yaml << EOF
+    apiVersion: v1
+    kind: Secret
+    metadata:
+        name: oauth-proxy-secret
+        namespace: oauth-proxy
+    stringData:
+        oidc-issuer-url: https://login.microsoftonline.com/$TENANT_ID/v2.0
+        stringData.scope: openid email
+        stringData.client-id: $CLIENT_ID
+        stringData.client-secret: $CLIENT_SECRET
+        stringData.cookie-secret: $randomSecretCookie
+        stringData.cookie-name: $secretName
+    EOF
+
+    kubectl apply -f oauth-proxy.yaml
+fi
+
 # Install helm package from ghcr
 helm upgrade --install causal-services $HELM_APP_LOCATION \
-    --set enableAuthentication=false,causalImagesPullPolicy=Always,causalImagesRegistry=$CAUSAL_REGISTRY,domain=$DOMAIN
+    --set enableAuthentication=$auth,causalImagesPullPolicy=Always,causalImagesRegistry=$CAUSAL_REGISTRY,domain=$DOMAIN
 
 # Add ingress
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
