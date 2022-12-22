f5dba65569f3e062cfd8714769659ad9c3942254
Dayenne de Souza <ddesouza@microsoft.com>
2022-12-22 18:21:05 -0300

    enable auth

--- docs/deployment/azure-scripts/helm-auth.sh
+++ docs/deployment/azure-scripts/helm-auth.sh
@@ -12,9 +12,7 @@ az aks install-cli
 # Get cluster credentials
 az aks get-credentials -g $RESOURCEGROUP -n $CLUSTER_NAME
 
-auth=false
-if [ -v $CLIENT_ID ];then
-    auth=true
+if $ENABLE_AUTH;then
     cookieName=_auth_token
 
     randomSecretCookie=`python -c 'import os,base64; print(base64.urlsafe_b64encode(os.urandom(32)).decode())'`
@@ -22,30 +20,31 @@ if [ -v $CLIENT_ID ];then
     # kubectl create namespace oauth-proxy
 
    cat > oauth-proxy.yaml <<EOF
-apiVersion:v1
-kind:Secret
+apiVersion: v1
+kind: Secret
 metadata:
-name:oauth-proxy-secret
-namespace:oauth-proxy
+    name: oauth-proxy-secret
+    namespace: oauth-proxy
 stringData:
-oidc-issuer-url:https://login.microsoftonline.com/$TENANT_ID/v2.0
-stringData.scope:openid email
-stringData.client-id:$CLIENT_ID
-stringData.client-secret:$CLIENT_SECRET
-stringData.cookie-secret:$randomSecretCookie
-stringData.cookie-name:$secretName
+    oidc-issuer-url: https://login.microsoftonline.com/$TENANT_ID/v2.0
+    scope: openid email
+    client-id: $CLIENT_ID
+    client-secret: $CLIENT_SECRET
+    cookie-secret: $randomSecretCookie
+    cookie-name: $cookieName
 EOF
 
+    kubectl create namespace oauth-proxy
     kubectl apply -f oauth-proxy.yaml
 fi
 
 # Install helm package from ghcr
 helm upgrade --install causal-services $HELM_APP_LOCATION \
-    --set enableAuthentication=$auth,causalImagesPullPolicy=Always,causalImagesRegistry=$CAUSAL_REGISTRY,domain=$DOMAIN
+    --set enableAuthentication=$ENABLE_AUTH,causalImagesPullPolicy=Always,causalImagesRegistry=$CAUSAL_REGISTRY,domain=$DOMAIN
 
 # Add ingress
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm install ingress-nginx ingress-nginx/ingress-nginx --create-namespace --namespace ingressnginx1 --set controller.service.loadBalancerIP=$PUBLICIP --set controller.service.externalTrafficPolicy=Local
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx --create-namespace --namespace ingressnginx1 --set controller.service.loadBalancerIP=$PUBLICIP --set controller.service.externalTrafficPolicy=Local
 
 echo \{\"Status\":\"Complete\"\} > $AZ_SCRIPTS_OUTPUT_PATH
