4ab58d57196c1b958d60e25e6bf056d8c72a109f
Monica Carvajal <mcarvajal@microsoft.com>
2022-12-27 20:01:56 -0500

    prepare analysis page

---
+++ javascript/app-event-analysis/src/pages/PrepareAnalysis/PrepareAnalysis.styles.ts
@@ -0,0 +1,24 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import { styled } from 'styled-components'
+
+export const DropdownContainer = styled.div`
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	gap: 0.5rem;
+	> * {
+		min-width: 0;
+	}
+`
+
+export const hypothesisGroupStyles = {
+	root: {
+		width: '100%',
+	},
+	flexContainer: {
+		display: 'flex',
+		justifyContent: 'space-between',
+	},
+}
