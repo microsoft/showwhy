720ae4707fb6abb87c5b6c567c09ea6a19600cff
Dayenne de Souza <ddesouza@microsoft.com>
2023-01-17 11:56:54 -0300

    fix variable bug when resetting or adding a new one

---
+++ javascript/app-model-exposure/src/utils/html.ts
@@ -0,0 +1,7 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+export function returnValidHTMLId(id: string): string {
+	return id.replace(/[^a-zA-Z0-9]/g, '')
+}
