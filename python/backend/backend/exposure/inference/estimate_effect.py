c92fb264990e0c329b098e14fc4e9324fbce248c
Rodrigo Racanicci <rracanicci@microsoft.com>
2023-01-18 17:38:38 -0300

    updating backend dependencies

--- python/backend/backend/exposure/inference/estimate_effect.py
+++ python/backend/backend/exposure/inference/estimate_effect.py
@@ -137,7 +137,7 @@ def estimate_effect(specification: Specification, task_id=None):
         identify_vars=True,
     )
 
-    identified_estimand = causal_model.identify_effect(proceed_when_unidentifiable=True, optimize_backdoor=True)
+    identified_estimand = causal_model.identify_effect(proceed_when_unidentifiable=True, optimize_backdoor=False)
     if identified_estimand.estimands["backdoor"] is None:
         return None
 
