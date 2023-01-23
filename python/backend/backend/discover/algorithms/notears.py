023e9924e698e9e6c2dce6f8f48034cabdd5ee5f
Rodrigo Racanicci <rracanicci@microsoft.com>
2023-01-23 14:50:34 -0300

    exposing more discover algorithms parameters in the backend

--- python/backend/backend/discover/algorithms/notears.py
+++ python/backend/backend/discover/algorithms/notears.py
@@ -1,3 +1,5 @@
+import logging
+
 from causalnex.structure.notears import from_pandas
 
 from backend.discover.algorithms.commons.base_runner import CausalDiscoveryRunner, CausalGraph, ProgressCallback
@@ -5,7 +7,7 @@ from backend.discover.model.causal_discovery import CausalDiscoveryPayload
 
 
 class NotearsPayload(CausalDiscoveryPayload):
-    pass
+    max_iter: int = 100
 
 
 class NotearsRunner(CausalDiscoveryRunner):
@@ -13,13 +15,21 @@ class NotearsRunner(CausalDiscoveryRunner):
 
     def __init__(self, p: NotearsPayload, progress_callback: ProgressCallback = None):
         super().__init__(p, progress_callback)
+        self._max_iter = p.max_iter
 
     def do_causal_discovery(self) -> CausalGraph:
         self._encode_categorical_as_integers()
 
+        logging.info(f"Running NOTEARS with max_iter={self._max_iter}, h_tol=1e-8 and w_threshold=0.0")
+
         causal_graph = self._build_causal_graph(
             labeled_graph=from_pandas(
-                self._prepared_data,
+                X=self._prepared_data,
+                max_iter=self._max_iter,
+                h_tol=1e-8,
+                # we can use w_threshold=0, since the weight
+                # filtering will be applied in the frontend
+                w_threshold=0.0,
                 tabu_child_nodes=self._constraints.causes,
                 tabu_parent_nodes=self._constraints.effects,
                 tabu_edges=self._constraints.forbiddenRelationships,
