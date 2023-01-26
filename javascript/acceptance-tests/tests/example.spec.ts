9adbe8dd4a35e479bbf58516e85999e9a3a3f873
Chris Trevino <darthtrevino@gmail.com>
2023-01-26 10:41:19 -0800

    add title test, config

--- javascript/acceptance-tests/tests/example.spec.ts
+++ javascript/acceptance-tests/tests/example.spec.ts
@@ -1,18 +1,9 @@
 import { test, expect } from '@playwright/test';
+import config from 'config'
+
+const TEST_URL = config.get<string>('test_url')
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  await page.goto(TEST_URL);
+  await expect(page).toHaveTitle(/ShowWhy Causal Platform/);
 });
