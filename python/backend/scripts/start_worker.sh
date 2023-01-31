3cc7676194488c881a716606481231f23ac1f750
Andres Morales <andresmor@microsoft.com>
2023-01-31 16:19:46 -0600

    Reduce docker file size
    Signed-off-by: Andres Morales <andresmor@microsoft.com>

--- python/backend/scripts/start_worker.sh
+++ python/backend/scripts/start_worker.sh
@@ -9,9 +9,9 @@ fi
 if [[ -z "${DEBUG}" ]]; then
     # worker debug disabled
     echo "Starting backend worker with debug disabled (extra_params=$extra_params)"
-    poetry run python -m celery -A backend.worker_main worker -l info $extra_params
+    ./.venv/bin/python -m celery -A backend.worker_main worker -l info $extra_params
 else
     # worker debug enabled
     echo "Starting backend worker with debug enabled (extra_params=$extra_params)"
-    poetry run python -m debugpy --listen 0.0.0.0:6901 -m celery -A backend.worker_main worker -l info $extra_params
+    ./.venv/bin/python -m debugpy --listen 0.0.0.0:6901 -m celery -A backend.worker_main worker -l info $extra_params
 fi
