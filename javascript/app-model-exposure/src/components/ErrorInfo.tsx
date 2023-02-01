0af6e1f15676f4caa384cf9c1761a2ad0c11c960
Monica Carvajal <mcarvajal@microsoft.com>
2023-02-01 17:15:58 -0500

    component to see the estimator errors log

--- javascript/app-model-exposure/src/components/ErrorInfo.tsx
+++ javascript/app-model-exposure/src/components/ErrorInfo.tsx
@@ -2,37 +2,63 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { Icon } from '@fluentui/react'
+import { ActionButton, Icon, type IModalStyles, Modal } from '@fluentui/react'
+import { useBoolean } from '@fluentui/react-hooks'
 import type { Theme } from '@thematic/core'
-import { memo } from 'react'
+import { memo, useMemo } from 'react'
 import styled from 'styled-components'
 
+import type { EstimatedEffect } from '../types/api/EstimateEffectStatus.js'
+import { JsonPreview } from './JsonPreview.js'
 import { ShortenMessage } from './ShortenMessage.js'
 
 export const ErrorInfo: React.FC<{
 	text?: string
+	errors?: EstimatedEffect[]
 	log?: string
 	styles?: React.CSSProperties
 	children?: React.ReactNode
 }> = memo(function ErrorInfo({
 	text = 'Undefined error, please try again.',
 	log = 'Undefined error, please try again.',
+	errors,
 	children,
 	styles,
 }) {
 	if (log) {
 		console.error('ErrorMessage:', log)
 	}
+	const [isModalOpen, { setTrue: showModal, setFalse: hideModal }] =
+		useBoolean(false)
+
+	const jsonError = useMemo((): string => {
+		return !errors ? '' : JSON.stringify(errors, null, 4)
+	}, [errors])
+
 	return (
 		<Container style={styles}>
 			<Error title={text}>
 				<Icon iconName="IncidentTriangle" />
 				{children}
 				{!children && text.length > 100 ? <ShortenMessage text={text} /> : text}
+				{!!errors && <SeeLogButton onClick={showModal} text="See log" />}
 			</Error>
+			<Modal
+				isOpen={isModalOpen}
+				onDismiss={hideModal}
+				isBlocking={false}
+				styles={modalStyles}
+			>
+				<ModalTitle>Errors log</ModalTitle>
+				<JsonPreview json={excToError(jsonError)} />
+			</Modal>
 		</Container>
 	)
 })
+
+function excToError(json: string): string {
+	return json.replaceAll('exc_info', 'error')
+}
 
 const Container = styled.p`
 	margin: 0;
@@ -42,3 +68,25 @@ const Container = styled.p`
 const Error = styled.small`
 	color: ${({ theme }: { theme: Theme }) => theme.application().error().hex()};
 `
+
+const SeeLogButton = styled(ActionButton)`
+	color: ${({ theme }) => theme.palette.themePrimary};
+	text-decoration: underline;
+	font-size: 11px;
+`
+
+const ModalTitle = styled.h3`
+	text-align: center;
+`
+
+const modalStyles = {
+	main: {
+		overflow: 'hidden',
+		maxHeight: '80vh',
+		width: '35vw',
+	},
+	scrollableContent: {
+		overflow: 'hidden',
+		padding: '1rem 1rem 5rem',
+	},
+} as IModalStyles
---
+++ javascript/app-model-exposure/src/components/JsonPreview.tsx
@@ -0,0 +1,69 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+
+import { IconButton } from '@fluentui/react'
+import { useBoolean } from '@fluentui/react-hooks'
+import { memo } from 'react'
+import styled from 'styled-components'
+
+export const JsonPreview: React.FC<{ json: string }> = memo(
+	function JsonPreview({ json }) {
+		const obj = JSON.parse(json)
+		const isArray = Array.isArray(obj)
+
+		return isArray ? (
+			<Container>
+				[
+				{obj.map((item, key) => (
+					<CollapsibleJson key={key} json={JSON.stringify(item, null, 4)} />
+				))}
+				]
+			</Container>
+		) : (
+			<CollapsibleJson json={json} />
+		)
+	},
+)
+
+export const CollapsibleJson: React.FC<{ json: string }> = memo(
+	function CollapsibleJson({ json }) {
+		const [isCollapsed, { toggle }] = useBoolean(false)
+		const iconProps = {
+			iconName: isCollapsed ? 'CaretRightSolid8' : 'CaretDownSolid8',
+		}
+		return (
+			<CollapsibleContainer isCollapsed={isCollapsed}>
+				<IconButton onClick={toggle} iconProps={iconProps} />
+				{isCollapsed ? (
+					<code>{json.slice(0, 50) + '...'}</code>
+				) : (
+					<pre>
+						<code>{json}</code>
+					</pre>
+				)}
+			</CollapsibleContainer>
+		)
+	},
+)
+
+const Container = styled.div`
+	overflow: auto;
+	height: 70vh;
+	padding: 1rem 0;
+`
+
+const CollapsibleContainer = styled.div<{ isCollapsed: boolean }>`
+	display: flex;
+	gap: 0.5rem;
+	align-items: ${({ isCollapsed }) => (isCollapsed ? 'center' : 'flex-start')};
+
+	pre {
+		margin: 0.5rem 0;
+	}
+
+	code {
+		font-size: 0.8rem;
+	}
+`
