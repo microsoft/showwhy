c0fa0361e66c12005b7cbe4a7e3521d50fb6ca36
Dayenne de Souza <ddesouza@microsoft.com>
2023-02-06 19:04:27 -0300

    fix scripts

---
+++ docs/deployment/azure/scripts/helm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Download and install Helm
+wget -O helm.tgz https://get.helm.sh/helm-v3.10.3-linux-amd64.tar.gz
+tar -zxvf helm.tgz
+mv linux-amd64/helm /usr/local/bin/helm
+
+# Install kubectl
+az account set --subscription $SUBSCRIPTION
+az aks install-cli
+# Get cluster credentials
+az aks get-credentials -g $RESOURCEGROUP -n $CLUSTER_NAME
+
+# Install helm package from ghcr
+helm upgrade --install causal-services $HELM_APP_LOCATION \
+    --set enableAuthentication=false,domain=$DOMAIN
+
+# Add ingress
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx --create-namespace --namespace ingressnginx1 --set controller.service.loadBalancerIP=$PUBLICIP --set controller.service.externalTrafficPolicy=Local
+
+echo \{\"Status\":\"Complete\"\} > $AZ_SCRIPTS_OUTPUT_PATH
